09dd50ac0cc692f78d2e9e56f85281a40f274f08
Katerina Walter <94501766+KaterinaWalter@users.noreply.github.com>
2025-04-17 11:35:44 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,33 @@
-console.log("hello world");
+// SECTION #1
+// Compare this code to the section below. Similarities? Differences? BE SPECIFIC!!!
+const highlighterButton = document.querySelector('#highlighterButton');
+highlighterButton.onclick = highlightVocab;
+function highlightVocab() {
+  const allVocabElements = document.querySelectorAll('.vocab');
+  for (const element of allVocabElements) {
+    element.classList.add('highlight'); 
+  }
+}
+
+// SECTION #2
+// Compare this code to the section above. Similarities? Differences? BE SPECIFIC!!!
+const styleButton = document.querySelector('#styleButton');
+styleButton.onclick = () => {
+  const firstParagraph = document.querySelector('.description');
+  firstParagraph.style.color = 'grey';
+  firstParagraph.style.fontSize = '20px';
+};
+
+// SECTION #3
+const clearButton = document.querySelector('#clearButton');
+clearButton.onclick = function() {
+  const allVocabElements = document.querySelectorAll('.vocab');
+  for (const element of allVocabElements) {
+    if (element.classList.contains('highlight')) {
+      element.classList.remove('highlight'); 
+    }
+  }
+  const firstParagraph = document.querySelector('.description');
+  firstParagraph.style.color = 'inherit';
+  firstParagraph.style.fontSize = 'inherit';
+};
